a022ce100b4ad69de10f2c9e014539c9839a7c23
Maico Roncancio <maico@gmail.com>
2025-04-02 11:33:14 -0500

    correccion error inicio sesion

--- frontend/script.js
+++ frontend/script.js
@@ -18,30 +18,6 @@ document.getElementById("registroForm").addEventListener("submit", async functio
     alert(data.message);
 });
 
-//inicio sesion 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("iniciosesionForm").addEventListener("submit", function(event) {
-        event.preventDefault(); // Evita que el formulario recargue la página
-
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-
-        fetch("http://localhost:3000/iniciosesion", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ email, password })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                window.location.href = "spacerent.html"; // Redirige si es exitoso
-            } else {
-                alert("Correo o contraseña incorrectos"); // Muestra error
-            }
-        })
-        .catch(error => console.error("Error:", error));
-    });
-});
 
 
 
